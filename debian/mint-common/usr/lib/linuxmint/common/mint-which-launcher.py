35a78ef702e973b44184040c5d1cebe2a8b3abb0
Clement Lefebvre <root@linuxmint.com>
2009-10-07 18:11:19 +0100

    Initial commit

---
+++ debian/mint-common/usr/lib/linuxmint/common/mint-which-launcher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+import sys
+import gettext
+
+gettext.install("messages", "/usr/lib/linuxmint/common/locale")
+
+launcher = "gksu  --message \"<b>" + _("Please enter your password") + "</b>\""
+if os.path.exists("/etc/linuxmint/info"):
+	sys.path.append('/usr/lib/linuxmint/common')
+	from configobj import ConfigObj
+	config = ConfigObj("/etc/linuxmint/info")
+	if (config['DESKTOP'] == "KDE"):
+		launcher = "kdesudo -i /usr/share/linuxmint/logo.png -d --comment \"<b>" + _("Please enter your password") + "</b>\""
+
+print launcher
+
+
+
