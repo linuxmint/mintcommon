d3bf81d37f84c35c110557c494a6b38c45c078bf
Clement Lefebvre <clem@dell.(none)>
2009-10-30 17:44:11 +0000

    Added launch_browser_as.py

---
+++ usr/lib/linuxmint/common/launch_browser_as.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys, os, commands
+
+link = ' '.join(sys.argv[1:])
+link = link.replace("\"", "")
+link = "\"" + link + "\""
+
+if os.path.exists("/usr/bin/gconftool-2"):
+	browser = commands.getoutput("gconftool-2 --get /desktop/gnome/url-handlers/http/command")		
+        browser = browser.replace("\"%s\"", link)
+	browser = browser.replace("%s", link) 		
+else:
+	browser = "firefox " + link	
+
+os.system(browser + " &")
+
