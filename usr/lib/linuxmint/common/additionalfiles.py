1064dad56eeac4686ed3b9cb0ace8907062c7049
Michael Webster <miketwebster@gmail.com>
2016-05-13 19:44:28 -0400

    additionalfiles.py: include keyword translations if provided

--- usr/lib/linuxmint/common/additionalfiles.py
+++ usr/lib/linuxmint/common/additionalfiles.py
@@ -3,8 +3,16 @@
 import os
 import gettext
 
+def strip_split_and_recombine(comma_separated):
+    word_list = comma_separated.split(",")
+    out = ""
+    for item in word_list:
+        out += item.strip()
+        out+=";"
 
-def generate(domain, path, filename, prefix, name, comment, suffix, genericName=None):
+    return out
+
+def generate(domain, path, filename, prefix, name, comment, suffix, genericName=None, keywords=None):
     gettext.install(domain, path)
     desktopFile = open(filename, "w")
 
@@ -31,6 +39,20 @@ def generate(domain, path, filename, prefix, name, comment, suffix, genericName=
                     desktopFile.writelines("Comment[%s]=%s\n" % (directory, _(comment)))
             except:
                 pass
+
+    if keywords is not None:
+        formatted = strip_split_and_recombine(keywords)
+        desktopFile.writelines("Keywords=%s\n" % formatted)
+        for directory in sorted(os.listdir(path)):
+            if os.path.isdir(os.path.join(path, directory)):
+                try:
+                    language = gettext.translation(domain, path, languages=[directory])
+                    language.install()
+                    if (_(keywords) != keywords):
+                        translated = strip_split_and_recombine(_(keywords))
+                        desktopFile.writelines("Keywords[%s]=%s\n" % (directory, translated))
+                except:
+                    pass
 
     if genericName is not None:
         desktopFile.writelines("GenericName=%s\n" % genericName)
